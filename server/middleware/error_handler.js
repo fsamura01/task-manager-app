e5c1b5d36312450099353bc3bd16e9ac757e0e46
Foday Samura <samura641@gmail.com>
2026-03-04 13:47:54 -0500

    chore: temporarily expose 500 error messages in production for debugging

--- server/middleware/error_handler.js
+++ server/middleware/error_handler.js
@@ -40,7 +40,7 @@ const sendErrorProd = (err, res) => {
     console.error("ERROR 💥", err);
     res.status(500).json({
       status: "error",
-      message: "Something went very wrong!",
+      message: err.message || "Something went very wrong!",
     });
   }
 };
